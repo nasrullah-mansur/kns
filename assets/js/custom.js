ad2f8068426b72ebfca1ae39367b2667be2d98c0
nasrullahmansur <nasrullahbd64@gmail.com>
2021-09-09 17:46:20 +0600

    3rd page done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,7 +17,22 @@ $(document).ready(function() {
     $('.header .header-content .mobile-btn').on('click', function(e) {
         e.preventDefault();
         $('.header .header-content .menu .top').slideToggle();
-    })
+    });
+
+
+    $('#tab-1').on('click', function() {
+        $('#content-2, #content-3').hide();
+        $('#content-1').show();
+    });
+
+    $('#tab-2').on('click', function() {
+        $('#content-1, #content-3').hide();
+        $('#content-2').show();
+    });
+    $('#tab-3').on('click', function() {
+        $('#content-2, #content-1').hide();
+        $('#content-3').show();
+    });
 
 });
 
